8e01ed437c1d96562bff217f124e159729e4982e
SpecialNoel <69883515+SpecialNoel@users.noreply.github.com>
2023-12-05 17:02:21 -0500

    Update tsh.cpp

--- src/tsh.cpp
+++ src/tsh.cpp
@@ -4,13 +4,18 @@
 #include <cstring>
 #include <unistd.h>
 #include <stdarg.h>
+#include <string>
+#include <ctime>
+#include <iomanip>
+#include <chrono>
+#include <bitset>
 
 using namespace std;
 
-void simple_shell::parse_command(char* cmd, CmdTokens** tokens) {
-  // Tokenize the command string into arguments     
+void simple_shell::parse_command(char* cmd, CmdTokens** tokens, int* tokenCount) {
+  // Tokenize the command string into arguments
 
-  // Checks if cmd is valid 
+  // Checks if cmd is valid
   size_t length = strlen(cmd);
   if (length > 0 && cmd[length - 1] == '\n') {
     cmd[length - 1] = '\0';
@@ -19,7 +24,6 @@ void simple_shell::parse_command(char* cmd, CmdTokens** tokens) {
   // Splits cmd via whitespace
   char *temp = strtok(cmd, " ");
   char **cmdTokens = (char**) malloc(sizeof(char));
-
   int i = 0;
   CmdTokens* curr = *tokens;
   while (temp != NULL) {
@@ -31,6 +35,7 @@ void simple_shell::parse_command(char* cmd, CmdTokens** tokens) {
       // Creating new struct & continueing to finalize data
       CmdTokens *t = (CmdTokens*) malloc(sizeof(CmdTokens*));
       curr->pipe = t;
+      *tokenCount += 1;
       // resetting data
       curr = t;
       cmdTokens = (char**) malloc(sizeof(char));
@@ -44,123 +49,146 @@ void simple_shell::parse_command(char* cmd, CmdTokens** tokens) {
   // Finalizing to final struct
   cmdTokens[i] = NULL;
   curr->cmd = cmdTokens;
+  *tokenCount += 1;
 }
 
-void simple_shell::exec_command(char** argv1, char** argv2) {
-  // Fork a child process to execute the command.
-  // Then return and pass result as input to the parent process.
-  int counter = 0;
-  for (char **temp = argv2; *temp; ++temp) {
-    counter++;
-  }
- 
-  if (counter != 0) {
-    int pipefd[2];
-    string cmdOp1 = argv1[0];
-    string cmdOp2 = argv2[0];
-    
-    if (pipe(pipefd) == 1) {
-      perror("Error: failed to create pipe\n.");
-      exit(1);
-    }
+void simple_shell::exec_command(CmdTokens* tokens, int tokenCount) {
+  // My tests: 
+  // ls
+  // ls | sort
+  // ls | sort | grep t
+  // cat src/tsh.cpp | grep string | grep c | grep f
 
-    // creating the first child to handle argv1
-    int pid = fork();
-    if (pid < 0) {
-      cout << "Fork failed to execute\n.";
-      exit(1);
-    } else if (pid == 0) {
-      // child uses write-end and closes read-end
-           
-      dup2(pipefd[1], STDOUT_FILENO);
-      close(pipefd[0]);
-      cout << cmdOp1 << endl;
-      execvp(cmdOp1.data(), argv1);
-
-      perror("execvp error in child 1");
-      exit(1);
-    } else {
-      // parent uses read-end and closes write-end
-
-      // creating the second child to handle argv2
-      int pid2 = fork();
-      if (pid2 < 0) {
-	cout << "Fork failed to execute in pid2\n.";
+  CmdTokens* curr = tokens;
+  int pipes[tokenCount-1][2];
+  int pids[tokenCount];
+  for (int i = 0; i < tokenCount; i++) {
+    char** args = curr->cmd;
+    // TODO: if final one, dont create
+    if (i != tokenCount-1) {
+      if (pipe(pipes[i]) == 1) {
+        perror("Error: failed to create pipe\n.");
         exit(1);
-      } else if (pid2 == 0) {
-        dup2(pipefd[0], STDIN_FILENO);
-        close(pipefd[1]);
-        execvp(cmdOp2.data(), argv2);
-	
-        perror("execvp error in child 2");
-        exit(1);
-      } else {
-	//parent
-	cout << "before" << endl;
-	int waiting = waitpid(pid, NULL, 0);
-	cout << "between" << endl;
-	int waiting2 = waitpid(pid2, NULL, 0);
-	cout << "after" << endl;
-	return;
       }
     }
-  } else {
-    string cmdOp1 = argv1[0];
-    int pid = fork();
-    if (pid < 0) {
+    
+    // creating the first child to handle argv1
+    pids[i] = fork();
+    if (pids[i] < 0) {
       cout << "Fork failed to execute\n.";
-      exit(1);  
-    } else if (pid == 0) {
-      //cout << "In child with 1 argv" << endl;
-      execvp(cmdOp1.data(), argv1);
-
-      perror("execvp error in child with 1 argv");
       exit(1);
-    } else {
-      //cout << "In parent with 1 argv" << endl;
-      int waiting = waitpid(pid, NULL, 0);
-      kill(pid, SIGQUIT);
+    } else if (pids[i] == 0) {
+      if (i != 0) {
+        //piping inputs
+        close(pipes[i-1][1]);
+        dup2(pipes[i-1][0], STDIN_FILENO);
+        close(pipes[i-1][0]);
+      }
+
+      if (i != tokenCount-1) {
+        // Piping outputs
+        close(pipes[i][0]);
+        dup2(pipes[i][1], STDOUT_FILENO);
+        close(pipes[i][1]);
+      }
+
+      execvp(args[0], args);
+      perror("execvp error in child 1");
+      exit(1);
     }
+
+    // closing previous' pipes
+    if (i != 0) {
+      close(pipes[i-1][0]);
+      close(pipes[i-1][1]);
+    }
+
+    curr = curr->pipe;
+  }
+
+  // Reap the children
+  for (int i = 0; i < tokenCount; i++) {
+    int waiting = waitpid(pids[i], NULL, 0);
   }
 }
 
-void simple_shell::printf_command(char**cmdTokens, ...) {
+void simple_shell::printf_command(char** cmdTokens, ...) {
     // Check if there are arguments after "printf"
     if (cmdTokens[1] == nullptr) {
-        std::cerr << "printf: missing arguments" << std::endl;
-        return;
+      std::cerr << "printf: missing arguments" << std::endl;
+      return;
     }
 
-    // Concatenate arguments into a single format string
-    string formatString;
-    for (int i = 1; cmdTokens[i] != nullptr; ++i) {
-        formatString += cmdTokens[i];
-        formatString += " ";
-    }
-        
-    // Call printf with the concatenated format string
+    std::string formatString;
     va_list args;
-    va_start(args, formatString.c_str());
+    va_start(args, cmdTokens);
+
+    // Collect arguments into a single format string
+    //std::string formatString;
+    for (int i = 1; cmdTokens[i] != nullptr; ++i) {
+      formatString += cmdTokens[i];
+      if (cmdTokens[i + 1] != nullptr) {
+        formatString += " ";  // Add space between words
+      }
+    }
+
+    // Process format extensions
+    size_t pos = 0;
+    int i =0;
+    while ((pos = formatString.find("%", pos)) != std::string::npos) {
+      if (formatString[pos + 1] == 'b') {
+        // Handle %b - expand backslash escape sequences
+        // Note: This assumes that cmdTokens[i + 1] is a string
+        std::string expanded;
+        expand_escape_sequences(cmdTokens[i + 1], expanded);
+        formatString.replace(pos, 16, expanded);
+      } else if (formatString[pos + 1] == 't') {
+        //cout << ctime(&timenow) <<  endl;
+        std::string timestamp;
+        get_current_timestamp(timestamp);
+        formatString.replace(pos, 2, timestamp);
+        //formatString += ctime(&timenow);
+      }
+      pos += 1;  // Move to the next position after the processed extension
+      i++;
+    }
+
+    pos = 0;
+    while ((pos = formatString.find("\\", pos)) != std::string::npos) {
+      // Handle \n - replace with newline character
+      if (formatString[pos + 1] == 'n') {
+        formatString.replace(pos, 2, "\n");
+      }
+      pos += 1;  // Move to the next position after the processed extension
+      i++;
+    }
+
+    // Call printf with the concatenated format string and variable arguments
+    /*va_list args;
+    va_start(args, cmdTokens[1]);
     vprintf(formatString.c_str(), args);
+    va_end(args);
+    */
+    vprintf(formatString.c_str(), args);
+
     va_end(args);
 }
 
 void simple_shell::help_command() {
-    // Provide information about available commands
-    cout << "Welcome to the simple shell!" << endl;
-    cout << "  printf to print" << endl;
-    cout << "  echo - Output provided arguments" << endl;
-    cout << "  help - Display this help message" << endl;
-    cout << "  quit - Exit the shell" << endl;
-    cout << "  read - Read and store one line from standard input" << endl;
-    // Add information about other built-in commands if needed
+  // Provide information about available commands
+  cout << "Welcome to the simple shell!" << endl;
+  cout << "  printf to print" << endl;
+  cout << "  help - Display this help message" << endl;
+  cout << "  echo - Output provided arguments" << endl;
+  cout << "  read - Read and store one line from standard input" << endl;
+  cout << "  quit - Exit the shell" << endl;
 }
 
 void simple_shell::read_command(char** cmdTokens, ...) {
   string formatString;
   for (int i = 1; cmdTokens[i] != nullptr; ++i) {
-      formatString += cmdTokens[i];
-      formatString += " ";
+    formatString += cmdTokens[i];
+    formatString += " ";
   }
   // Store the provided read variable in the second value of read_line pair in simple_shell class
   read_line.second = formatString;
@@ -170,27 +198,27 @@ void simple_shell::read_command(char** cmdTokens, ...) {
 
 void simple_shell::echo_command(char** cmdTokens, ...) {
   // Check if there are arguments after "echo"
-    if (cmdTokens[1] == nullptr) {
-        std::cerr << "echo: missing arguments" << std::endl;
-        return;
-    }
+  if (cmdTokens[1] == nullptr) {
+    std::cerr << "echo: missing arguments" << std::endl;
+    return;
+  }
 
-    // Concatenate arguments into a single format string
-    string formatString;
-    for (int i = 1; cmdTokens[i] != nullptr; ++i) {
-        formatString += cmdTokens[i];
-        formatString += " ";
-    }
-    // Check if the echo argument is a stored variable --> print stored variable, "$REPLY" used if no read variable was provided. Else print out provided argument.
-    if((formatString == "$" + read_line.second || formatString == "$REPLY ") && formatString[0] == '$'){
-      cout << read_line.first << endl;
-    } 
-    else {
-      va_list args;
-      va_start(args, formatString.c_str());
-      vprintf(formatString.c_str(), args);
-      va_end(args);
-    }
+  // Concatenate arguments into a single format string
+  string formatString;
+  for (int i = 1; cmdTokens[i] != nullptr; ++i) {
+    formatString += cmdTokens[i];
+    formatString += " ";
+  }
+  // Check if the echo argument is a stored variable --> print stored variable,
+  // "$REPLY" used if no read variable was provided. Else print out provided argument.
+  if((formatString == "$" + read_line.second || formatString == "$REPLY ") && formatString[0] == '$'){
+    cout << read_line.first << endl;
+  } else {
+    va_list args;
+    va_start(args, formatString.c_str());
+    vprintf(formatString.c_str(), args);
+    va_end(args);
+  }
 }
 
 bool simple_shell::isQuit(char* cmd) {
@@ -201,14 +229,14 @@ bool simple_shell::isQuit(char* cmd) {
 }
 
 bool simple_shell::isHelp(char* cmd){
-  // check for the command  "help", shell will reponse accordingly
+  // check for the command "help", shell will reponse accordingly
   string cmdStr = cmd;
   string helpCommand = "help";
   return (cmdStr.compare(helpCommand)==0);
 }
 
 bool simple_shell::isPrintf(char* cmd){
-  // check for the command  "printf", shell will reponse accordingly
+  // check for the command "printf", shell will reponse accordingly
   string cmdStr = cmd;
   string PrintfCommand = "printf";
   return (cmdStr.compare(PrintfCommand)==0);
@@ -226,4 +254,28 @@ bool simple_shell::isEcho(char* cmd) {
   string cmdStr = cmd;
   string echoCommand = "echo";
   return(cmdStr.compare(echoCommand) == 0);
+}
+
+void simple_shell::expand_escape_sequences(const std::string& input, std::string& output) {
+  output.clear();  // Clear the output string before appending characters
+  std::ostringstream oss;
+  for (size_t i = 0; i < input.size(); ++i) {
+    // Check for escape sequences
+    std::bitset<8>binaryRepresentation(input[i]);
+    oss << binaryRepresentation.to_string();
+  }
+  output = oss.str();
+}
+
+void simple_shell::get_current_timestamp(std::string& output) {
+  // Get the current timestamp in seconds since the epoch
+  time_t current_time = std::time(nullptr);
+
+  // Format the current timestamp using strftime
+  char buffer[256]; // Adjust the buffer size as needed
+  std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", std::localtime(&current_time));
+
+  // Assign the formatted string to the output
+  output = buffer;
+  // return output;
 }
