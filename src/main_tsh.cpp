7fd2cbf4adb13f6b0637589937b66ed77ef86d37
SpecialNoel <69883515+SpecialNoel@users.noreply.github.com>
2023-12-05 15:37:24 -0500

    Update main_tsh.cpp

--- src/main_tsh.cpp
+++ src/main_tsh.cpp
@@ -4,30 +4,26 @@ using namespace std;
 
 int main() {
   char cmd[81];
-  CmdTokens *tokens = new CmdTokens();
   simple_shell *shell = new simple_shell();
   
   cout << "tsh> ";
   while (fgets(cmd, sizeof(cmd), stdin)) {
+    CmdTokens *tokens = new CmdTokens();
     if (cmd[0] != '\n') {
       shell->parse_command(cmd, &tokens);
       
       if (shell->isQuit(tokens->cmd[0])) {                                                 
         exit(0);
       } else if (shell->isPrintf(tokens->cmd[0])) {
-	      shell->printf_command(tokens->cmd);
-	      cout << endl;
+	shell->printf_command(tokens->cmd);
+	cout << endl;
       } else if (shell->isHelp(tokens->cmd[0])) {
-	      shell->help_command();
-      } else if (shell->isRead(tokens->cmd[0])) {
-        shell->read_command(tokens->cmd);
-      } else if (shell->isEcho(tokens->cmd[0])) {
-        shell->echo_command(tokens->cmd);
-        cout << endl;
-      } 
-      else {
-	      // only this command supports pipe commands
-        // shell->exec_command(cmdTokens1, cmdTokens2);
+	shell->help_command();
+      } else if (shell->isAlias(tokens->cmd[0])) {
+	shell->alias_command(tokens->cmd);
+      } else {
+	// only this command supports pipe commands
+        shell->exec_command(tokens);
       }
       
     }
